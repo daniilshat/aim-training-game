49f2cbdd287323fbcaa7b7555c437a59e35d8efa
daniilshat <daniilshstukhin@gmail.com>
2021-04-05 18:41:33 +0300

    Final version 1.0
    Added:
    - 4 types of targets;
    - different colors for different types of targets;
    - balanced spawn speed;
    - points and health system;
    - points and health screening in the window.

--- Game.h
+++ Game.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include <sstream>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
@@ -21,12 +24,34 @@ private:
     sf::VideoMode videoMode;
     sf::Event ev;
 
+    //Mouse position
+    sf::Vector2i mousePosWindow;
+    sf::Vector2f mousePosView;
+
+    //Resources
+    sf::Font font;
+
+    //Text
+    sf::Text uiText;
+
+    //Game logic
+    bool endGame;
+    unsigned points;
+    int health;
+    float enemySpawnTimer;
+    float enemySpawnTimerMax;
+    int maxEnemies;
+    bool mouseHeld;
+
     //Game objects
+    std::vector<sf::RectangleShape> enemies;
     sf::RectangleShape enemy;
 
     //Private functions
     void initVariables();
     void initWindow();
+    void initFonts();
+    void initText();
     void initEnemies();
 
 public:
@@ -36,10 +61,18 @@ public:
 
     //Accessors
     const bool running() const;
-
+    const bool getEndGame() const;
 
     //functions
+    void spawnEnemies();
+
     void pollEvents();
+    void updateMousePositions();
+    void updateText();
+    void updateEnemies();
     void update();
+
+    void renderText(sf::RenderTarget& target);
+    void renderEnemies(sf::RenderTarget& target);
     void render();
 };
--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 #include "Game.h"
 
 int main() {
+    //Init srand
+    std::srand(static_cast<unsigned>(time(NULL)));
+
     //Init Game engine
     Game game;
 
     //Game loop
-    while (game.running()) {
+    while (game.running() && !game.getEndGame()) {
 
         //Update
         game.update();
